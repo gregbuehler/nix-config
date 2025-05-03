e336ff00cd45dcbaf5f0a970a287297fb4199373
gregbuehler <gregory.r.buehler@gmail.com>
2025-05-02 23:54:07 -0700

    nvidia

--- hosts/eternia/hardware-configuration.nix
+++ hosts/eternia/hardware-configuration.nix
@@ -2,7 +2,9 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [ 
+      (./. + "/nvidia.nix")
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot = {
--- hosts/eternia/user.nix
+++ hosts/eternia/user.nix
@@ -3,7 +3,6 @@
 {
     imports = [ 
         ../../modules/default.nix 
-        ./nvidia.nix
     ];
     config.modules = {
         # gui
